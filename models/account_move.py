bceae9f47780885417b50d943c329644edfa3078
Tony Galmiche <tony.galmiche@infosaone.com>
2024-12-28 12:15:22 +0100

    modifs

--- models/account_move.py
+++ models/account_move.py
@@ -54,5 +54,8 @@ class AccountMove(models.Model):
             return res
 
 
+class AccountMoveLine(models.Model):
+    _inherit = "account.move.line"
 
+    is_account_invoice_line_id = fields.Integer('Lien entre account_invoice_line et account_move_line pour la migration', index=True)
 
